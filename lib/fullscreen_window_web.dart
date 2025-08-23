73abc3738c2790671822cb72672de4d5e07e9a93
zhuhaichao518 <zhuhaichao518@gmail.com>
2025-08-24 06:33:34 +0800

    [web]支持flutter新特性WASM编译

--- lib/fullscreen_window_web.dart
+++ lib/fullscreen_window_web.dart
@@ -2,13 +2,44 @@
 // of your plugin as a separate package, instead of inlining it in the same
 // package as the core of your plugin.
 import 'package:web/web.dart' as web show window;
-import 'dart:html';
-import 'dart:js_util' as js_util;
+import 'dart:js_interop';
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'fullscreen_window_platform_interface.dart';
+
+@JS()
+external Window get window;
+
+@JS()
+@staticInterop
+class Window {}
+
+extension WindowExtension on Window {
+  @JS('navigator')
+  external WebNavigator get navigator;
+}
+
+@JS()
+@staticInterop
+class WebNavigator {}
+
+extension WebNavigatorExtension on WebNavigator {
+  @JS('keyboard')
+  external Keyboard get keyboard;
+}
+
+@JS()
+@staticInterop
+class Keyboard {}
+
+extension KeyboardExtension on Keyboard {
+  @JS('lock')
+  external JSPromise lock([JSArray keyCodes]);
+  @JS('unlock')
+  external JSVoid unlock();
+}
 
 /// A web implementation of the FullscreenWindowPlatform of the FullscreenWindow plugin.
 class FullScreenWindowWeb extends FullScreenWindowPlatform {
@@ -24,14 +55,10 @@ class FullScreenWindowWeb extends FullScreenWindowPlatform {
   Future<void> setFullScreen(bool isFullScreen) async {
     if (isFullScreen) {
       web.window.document.documentElement?.requestFullscreen();
-      final navigator = js_util.getProperty(window, 'navigator');
-      final keyboard = js_util.getProperty(navigator, 'keyboard');
-      // js_util.callMethod(keyboard, 'lock', [
-      //   ['Escape'] // 锁定 ESC 键
-      // ]);
-      js_util.callMethod(keyboard, 'lock', []);
+      window.navigator.keyboard.lock();
     } else {
       web.window.document.exitFullscreen();
+      window.navigator.keyboard.unlock();
     }
   }
 
